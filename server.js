059df6418f173585496d071cb6c342e07d03553e
keeganthomp <whereiskeegan@gmail.com>
2017-07-06 14:39:08 -0500

    adding the ability to add and update pokemon from the db

--- server.js
+++ server.js
@@ -4,18 +4,17 @@ const bodyParser = require("body-parser");
 mongoose.Promise = require("bluebird");
 const mustache = require("mustache-express");
 const dbUrl = "mongodb://localhost:27017/pokemon";
-const Pokemon = require('./models/Pokemon');
+const Pokemon = require("./models/Pokemon");
 const app = express();
-const port = process.env.PORT || 8000;                                  
+const port = process.env.PORT || 8000;
 
 app.engine("mustache", mustache());
 app.set("view engine", "mustache");
 app.set("views", "./views");
 
-
 //MIDDLEWARE
 app.use(bodyParser.json());
-
+app.use(bodyParser.urlencoded({ extended: false }));
 
 //DB CONNECTION
 mongoose.connect(dbUrl).then((err, db) => {
@@ -25,19 +24,53 @@ mongoose.connect(dbUrl).then((err, db) => {
   console.log("Connected to the DB");
 });
 
-
 //ROUTES
-app.get("/pokemon", (req, res)=>{
-    pokemon.find().then(foundPokes=>{
-        res.send(foundPokes);
-    }).catch(err =>{
-        res.send(err);
+app.get("/pokemon", (req, res) => {
+  pokemon
+    .find()
+    .then(foundPokes => {
+      res.send(foundPokes);
     })
-})
+    .catch(err => {
+      res.send(err);
+    });
+});
 
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
+app.post("/pokemon", (req, res) => {
+  let pokemonData = req.body;
+  console.log(pokemonData);
+  let newPokemon = new Pokemon(pokemonData);
+  newPokemon
+    .save()
+    .then(savedPokemon => {
+      res.send(savedPokemon);
+    })
+    .catch(err => {
+      res.send(err);
+    });
+});
 
+app.get("/pokemon/:name", (req, res) => {
+  Pokemon
+    .findOne({ name: req.params.name }, req.body)
+    .then(foundPokemon => {
+      res.render("updatePokemon", {foundPokemon: foundPokemon});
+    });
+});
 
+app.post("/pokemon/:id", (req, res) => {
+  Pokemon.updateOne({ _id: req.params.id }, req.body)
+    .then(updatedPokemon => {
+      res.send(updatedPokemon);
+    })
+    .catch(err => {
+      res.status(500).send(err);
+    });
+});
 
 //LISTEN
 app.listen(port, () => {
